1dd32fd164e1d4a5b825e5bc52525fcd106bfd64
ivan <ivanprikhodko2004@gmail.com>
2023-03-04 12:32:16 +0300

    step six

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -7,13 +7,19 @@ import hexlet.code.models.query.QUrlCheck;
 import io.javalin.http.Handler;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClients;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class UrlsController {
     private static int getIdFromPath(String path) {
@@ -77,7 +83,14 @@ public class UrlsController {
         List<UrlCheck> urlChecks = new QUrlCheck()
                 .url.eq(findUrl)
                 .findList();
-
+        urlChecks = urlChecks.stream()
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        lst -> {
+                            Collections.reverse(lst);
+                            return lst.stream();
+                        }
+                ))
+                .toList();
         ctx.attribute("url", findUrl);
         if(urlChecks != null && !urlChecks.isEmpty()) {
             ctx.attribute("urlsCheck", urlChecks);
@@ -87,30 +100,33 @@ public class UrlsController {
 
     private static String getTitle(HttpResponse responsePost) {
         String body = responsePost.getBody().toString();
-        Pattern pattern = Pattern.compile("(?<=<title.{0,}>).{1,}(?=<\\/title>)");
+        //Pattern pattern = Pattern.compile("(?<=<title.{0,}>).{1,}(?=<\\/title>)", Pattern.DOTALL);
+        Pattern pattern = Pattern.compile("<title.{0,}>.{1,}?<\\/title>");
         Matcher matcher = pattern.matcher(body);
         if(matcher.find()){
-            return body.substring(matcher.start(), matcher.end()).replaceAll("<.{0,}>", "");
+            return body.substring(matcher.start(), matcher.end()).replaceAll("(<title.*?>|<\\/title>)", "");
         }
         return "";
     }
 
     private static String getH1(HttpResponse responsePost) {
         String body = responsePost.getBody().toString();
-        Pattern pattern = Pattern.compile("(?<=<h1.{0,}>).{1,}(?=<\\/h1>)");
+//        Pattern pattern = Pattern.compile("(?<=<h1.{0,}>).{1,}(?=<\\/h1>)");
+        Pattern pattern = Pattern.compile("<h1.{0,}?>.{1,}?<\\/h1>");
         Matcher matcher = pattern.matcher(body);
         if(matcher.find()){
-            return body.substring(matcher.start(), matcher.end()).replaceAll("<.{0,}>", "");
+            return body.substring(matcher.start(), matcher.end()).replaceAll("(<h1.*?>|<\\/h1>)", "");
         }
         return "";
     }
 
     private static String getDescription(HttpResponse responsePost) {
         String body = responsePost.getBody().toString();
-        Pattern pattern = Pattern.compile("(?<=<meta name=\"description\" content=\").+?(?=\" \\/>)");
+//        Pattern pattern = Pattern.compile("(?<=<meta name=\"description\" content=\").+?(?=\" \\/>)");
+        Pattern pattern = Pattern.compile("<meta name=\"description\" content=\".+?\" \\/>");
         Matcher matcher = pattern.matcher(body);
-        if(matcher.find()){
-            return body.substring(matcher.start(), matcher.end()).replaceAll("<.{0,}>", "");
+        if(matcher.find()) {
+            return body.substring(matcher.start(), matcher.end()).replaceAll("(<meta name=\"description\" content=\"|\" \\/>)", "");
         }
         return "";
     }
@@ -122,12 +138,18 @@ public class UrlsController {
                 .findOne();
         String name = findUrl.getName();
 
+        HttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+        Unirest.config().httpClient(httpClient);
         HttpResponse<String> responsePost = Unirest
                 .get(name)
                 .asString();
 
 
         UrlCheck urlCheck = new UrlCheck(responsePost.getStatus(), getTitle(responsePost), getH1(responsePost), getDescription(responsePost), findUrl);
+
         findUrl.getUrlChecks().add(urlCheck);
         urlCheck.save();
         ctx.sessionAttribute("flash", "Страница успешно проверена");
